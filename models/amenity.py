af3f1e88c27c00af7bbdfaef06d10d24d3e780a1
edisson11osorio <osorioedisson08@gmail.com>
2022-05-14 21:54:21 -0500

    update

--- models/amenity.py
+++ models/amenity.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+
+import os
 from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel):
