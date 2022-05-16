66e2eff8ce0028e1933a615f0c7680567e38aaf9
edisson11osorio <osorioedisson08@gmail.com>
2022-05-16 10:45:59 -0500

    city_update

--- models/city.py
+++ models/city.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+import models
+from models.base_model import BaseModel, Base
+from models.place import Place
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel):
