4b407e6118c253c3e3452518bd380335307313bb
edisson11osorio <osorioedisson08@gmail.com>
2022-05-16 21:24:23 -0500

    ameni

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-
-import os
+import models
 from models.base_model import BaseModel
 from models.base_model import Base
 from sqlalchemy import Column
@@ -9,5 +8,15 @@ from sqlalchemy import String
 from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """ Amenity: Class"""
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary="place_amenity",
+                                   viewonly=False)
+
+    def __init__(self, *args, **kwargs):
+        """
+            Init for inherited
+        """
+        super().__init__(*args, **kwargs)
