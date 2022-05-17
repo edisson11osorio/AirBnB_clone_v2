6dfc36aeb468de4854d6c17369d23eb008485e56
edisson11osorio <osorioedisson08@gmail.com>
2022-05-16 21:27:01 -0500

    re

--- models/review.py
+++ models/review.py
@@ -5,6 +5,13 @@ from models.base_model import BaseModel
 
 class Review(BaseModel):
     """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+    __tablename__ = "reviews"
+    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    text = Column(String(1024), nullable=False)
+
+    def __init__(self, *args, **kwargs):
+        """
+            Init for inherited
+        """
+        super().__init__(*args, **kwargs)
