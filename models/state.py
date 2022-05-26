e5c7e57b456a8cd514d694afb2100199f3895e75
edisson11osorio <osorioedisson08@gmail.com>
2022-05-25 23:30:48 -0500

    fix 🐛

--- models/state.py
+++ models/state.py
@@ -8,9 +8,9 @@ from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    __tablename__ = 'states'
+    __tablename__ = "states"
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state")
 
@@ -24,8 +24,8 @@ class State(BaseModel):
         @property
         def cities(self):
             """getter for cities that return
-            a list of city instance equal to
-            current state id
+            a list of city instance equale to
+            curent state id
             """
             list_city = []
             all_inst_c = models.storage.all(City)
